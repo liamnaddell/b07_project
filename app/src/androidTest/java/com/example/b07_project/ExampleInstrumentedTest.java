5605a1bdfd53e102edf3b24e54d3e5aec6f7d399
Liam Naddell <liamnprg@gmail.com>
2022-07-25 11:33:59 -0400

    rename project from b09 to b07

--- app/src/androidTest/java/com/example/b07_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/b07_project/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.b09_project;
+package com.example.b07_project;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.b09_project", appContext.getPackageName());
+        assertEquals("com.example.b07_project", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/b07_project/MainActivity.java
+++ app/src/main/java/com/example/b07_project/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.b09_project;
+package com.example.b07_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
