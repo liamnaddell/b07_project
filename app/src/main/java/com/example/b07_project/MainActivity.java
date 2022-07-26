d384dc3d4f324bff946fd28984fc58e85d137984
Liam Naddell <liamnprg@gmail.com>
2022-07-25 20:14:07 -0400

    create different view templates and hook tharuths code up to the mainActivity

--- app/src/main/java/com/example/b07_project/MainActivity.java
+++ app/src/main/java/com/example/b07_project/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.b07_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void login_handler(View view) {
+        System.out.println("login handler");
+        Intent intent = new Intent(this, LoginPage.class);
+        startActivity(intent);
+    }
+    public void venues_handler(View view) {
+        System.out.println("venues handler");
+    }
+    public void event_handler(View view) {
+        System.out.println("event handler");
+    }
+    public void user_handler(View view) {
+        System.out.println("user handler");
+    }
+
 }
