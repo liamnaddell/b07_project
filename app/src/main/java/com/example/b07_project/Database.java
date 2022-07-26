eec4ee5699b9b543373e1339f0ec14bb1ca163ee
Liam Naddell <liamnprg@gmail.com>
2022-07-25 22:20:43 -0400

    tiny db fix

--- app/src/main/java/com/example/b07_project/Database.java
+++ app/src/main/java/com/example/b07_project/Database.java
@@ -1,5 +1,6 @@
 package com.example.b07_project;
 
 public interface Database {
-    boolean check_pw(String username, String password);
+    public boolean check_pw(String username, String password);
+    public void add_user(String username, String password);
 }
