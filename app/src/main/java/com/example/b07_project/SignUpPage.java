d2be5aea69331151775185349a7fe58d737f2677
Liam Naddell <liamnprg@gmail.com>
2022-08-09 18:53:45 -0400

    small signup fix

--- app/src/main/java/com/example/b07_project/SignUpPage.java
+++ app/src/main/java/com/example/b07_project/SignUpPage.java
@@ -22,7 +22,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthMultiFactorException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.ktx.Firebase;
